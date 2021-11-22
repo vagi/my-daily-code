7605fce502bf5ecbd7fca3d34049b96da484e9d9
Vagif Molla Air <vagif69@gmail.com>
2021-11-22 19:28:39 +0200

    Correct final version

--- permutations_in_list.py
+++ permutations_in_list.py
@@ -1,3 +1,5 @@
+# Source: https://stepik.org/lesson/415554/step/3?unit=405083
+
 numbers = [int(num) for num in input().split()]
 if len(numbers) % 2 == 0:
     for idx in range(0, len(numbers), 2):
@@ -6,4 +8,7 @@ else:
     for idx in range(0, len(numbers[:-1]), 2):
         numbers[idx], numbers[idx+1] = numbers[idx+1], numbers[idx]
 
-print(numbers)
+print(*numbers)    # Unpacking the list object - *
+
+
+# Input: 1 2 3 4 5
