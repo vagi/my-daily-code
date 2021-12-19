ce40b74ac9ec98347eee18260a41614f64ea5230
Vagif Molla Air <vagif69@gmail.com>
2021-12-19 19:52:20 +0200

    Add processing of ZeroDivision exception

--- gcd.py
+++ gcd.py
@@ -1,4 +1,6 @@
 # Greatest common divisor by Euclidian algorithm
+from typing import Callable
+
 
 def get_gcd(x: int, y: int) -> int:
     """
@@ -7,19 +9,25 @@ def get_gcd(x: int, y: int) -> int:
     """
     if x < y:
         x, y = y, x
-    while y != 0:
-        x, y = y, x % y
-    return x
+
+    try:
+        x % y
+        while y:
+            x, y = y, x % y
+        return x
+    except ZeroDivisionError:
+        print("It's not possible to divide by 0")
 
 
-# print(f"GCD = {get_gcd(15, 64)}")
+# print(f"GCD = {get_gcd(0, 75)}")
 
 
-def test_gcd_func(func: object, x: int, y: int, gcd_out: int) -> int:
+def test_gcd_func(func: Callable[[int, int], int], x: int, y: int, gcd_out: [int, None]) -> int:
     test_out = func(x, y) == gcd_out
     return test_out
 
+
 print(f"Test 01 result: {test_gcd_func(get_gcd, 21, 35, 7)}")
-print(f"Test 02 result: {test_gcd_func(get_gcd, 327, 801, 3)}")
+print(f"Test 02 result: {test_gcd_func(get_gcd, 0, 73, None)}")
 print(f"Test 03 result: {test_gcd_func(get_gcd, 26, 260, 26)}")
 print(f"Test 04 result: {test_gcd_func(get_gcd, 43, 433, 1)}")
